598873d793c116ac1d635ef748cbf90ff76df733
poorvagadre <purvaagadre@gmail.com>
2021-03-22 16:36:44 +0530

    Function headers

--- math.py
+++ math.py
@@ -1,8 +1,12 @@
+#ADD IMPLEMENTATION
 def add(x,y):
     pass
+#sUBTRACT IMPLEMENTATION
 def subtract(x,y):
     pass
+#MULTIPLY IMPLEMENTATION
 def multiply(x,y):
     pass
+#DIVIDE IMPLEMENTATION
 def divide(x,y):
     pass
